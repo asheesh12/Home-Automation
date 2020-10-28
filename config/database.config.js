4a35259773975d2844b34c5450c5e7465de6f202
Asheesh Bhuria <asheesh.bhuria@gmail.com>
2020-10-28 21:17:55 +0530

    Add database configuration file

---
+++ config/database.config.js
@@ -0,0 +1,29 @@
+'use strict';
+ 
+const config = require('./config.js');
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+ 
+const connectionString = config.dbServer;
+
+const connection = mongoose.createConnection(connectionString);
+connection.on('error', function() {
+    console.error(chalk.red('Mongo database connection error when connecting to ' + connectionString + '...'));
+});
+connection.once('open', function() {
+    console.log(chalk.green('Mongo database connection opened for: ' + connectionString));
+});
+ 
+const dbConnection = connection.useDb('homeAutomation');
+
+exports.addModel = addModel;
+exports.getModel = getModel;
+exports.Schema = mongoose.Schema;
+
+function addModel(schemaName, schema) {
+    dbConnection.model(schemaName, schema);
+}
+ 
+function getModel(schemaName) {
+    return dbConnection.model(schemaName);
+}
