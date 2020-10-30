943f7b16d660acba34141c796cc74074ce460d7e
Asheesh Bhuria <asheesh.bhuria@gmail.com>
2020-10-30 22:34:22 +0530

    Fix issues in name

--- routes/devices.route.js
+++ routes/devices.route.js
@@ -4,9 +4,9 @@ const DeviceController = require('../controllers/devices.controller');
 const { pairedDeviceValidators, addDeviceValidators, updateDeviceValidators, removeDeviceValidators } = require('../controllers/helpers/devices.validator');
 const { validateRequest } = require('../services/validator');
 
-router.get('/available/:houseIds', pairedDeviceValidators, validateRequest,  DeviceController.getDevices);
+router.get('/available/:houseIds', pairedDeviceValidators, validateRequest,  DeviceController.getPairedDevices);
 router.post('/', addDeviceValidators, validateRequest, DeviceController.addDevice);
 router.put('/:deviceId', updateDeviceValidators, validateRequest, DeviceController.updateDevice);
-router.delete('/deviceId',removeDeviceValidators, validateRequest, DeviceController.updateDevice);
+router.delete('/deviceId',removeDeviceValidators, validateRequest, DeviceController.removeDevice);
 
 module.exports = router;
